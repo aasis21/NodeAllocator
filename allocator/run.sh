b66be32b9c21875a5f7a310cf2a0fcf0f4dcd341
NamanJain8 <jnaman806@gmail.com>
2019-09-07 01:22:35 +0530

    argparse in binary

--- allocator/run.sh
+++ allocator/run.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+array="${@}"
 proc_count=$1
-binary=$3
+binary=${array[@]:3}
+echo $binary
 ppn=$2
 ./src/min_diameter.out $proc_count $ppn
 echo ""
@@ -14,9 +16,9 @@ echo "MPI PROGRAM STARTS : $start "
 echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
 if (( $ppn == 0 ))
 then
-   mpiexec -n $proc_count -hostfile hosts $binary >> out.tmp
+   mpiexec -n $proc_count -hostfile hosts $binary
 else
-    mpiexec -n $proc_count -ppn $ppn -hostfile hosts $binary >> out.tmp
+    mpiexec -n $proc_count -ppn $ppn -hostfile hosts $binary
 fi
 end=`date +%s`
 runtime=$((end-start))
@@ -24,34 +26,8 @@ echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
 echo "MPI PROGRAM ENDS : $end :: Total Time : $runtime"
 echo "======================================================================"
 
-
 numhosts=`wc -l < hosts`
-numlivehosts=`wc -l < /users/btech/akashish/.eagle/livehosts.txt`
-selectedend=`shuf -i $numhosts-$numlivehosts -n 1`
-cat /users/btech/akashish/.eagle/livehosts.txt | head -n $selectedend | tail -n $numhosts > sequencehosts
-echo ""
-echo ""
-echo "Random sequence Selected hosts are"
-cat sequencehosts
-echo "======================================================================"
-start=`date +%s`
-echo "MPI PROGRAM STARTS ON SEQUENCE : $start "
-echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
-if (( $ppn == 0 ))
-then
-   mpiexec -n $proc_count -hostfile sequencehosts $binary >> out.tmp
-else
-    mpiexec -n $proc_count -ppn $ppn -hostfile sequencehosts $binary  >> out.tmp
-fi
-end=`date +%s`
-runtime=$((end-start))
-echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
-echo "MPI PROGRAM ENDS : $end :: Total Time : $runtime"
-echo "======================================================================"
-
-
-numhosts=`wc -l < hosts`
-cat ~/.eagle/livehosts.txt | sort -R | sort -R | head -n $numhosts > randomhosts
+cat ~/.eagle/livehosts.txt | sort -R | head -n $numhosts > randomhosts
 echo ""
 echo ""
 echo "Randomly Selected hosts are"
@@ -62,13 +38,12 @@ echo "MPI PROGRAM STARTS ON RANDOM : $start "
 echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
 if (( $ppn == 0 ))
 then
-   mpiexec -n $proc_count -hostfile randomhosts $binary >> out.tmp
+   mpiexec -n $proc_count -hostfile randomhosts $binary
 else
-    mpiexec -n $proc_count -ppn $ppn -hostfile randomhosts $binary >> out.tmp
+    mpiexec -n $proc_count -ppn $ppn -hostfile randomhosts $binary
 fi
 end=`date +%s`
 runtime=$((end-start))
 echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
 echo "MPI PROGRAM ENDS : $end :: Total Time : $runtime"
 echo "======================================================================"
-
