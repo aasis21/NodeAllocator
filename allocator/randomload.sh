20aaa508795482ad2d0605b072f307b9ee4bdeb1
NamanJain8 <jnaman806@gmail.com>
2019-09-07 00:29:15 +0530

    random load added

--- allocator/randomload.sh
+++ allocator/randomload.sh
@@ -1,26 +1,30 @@
 #!/bin/bash
-numhosts=$1
-ppn=$2
-proc_count=$3
-binary=$4
-cat ~/.eagle/livehosts.txt | sort -R | sort -R | head -n $numhosts > loadhosts
-echo ""
-echo ""
-echo "Randomly Selected hosts for dummy load"
-cat loadhosts
-echo "======================================================================"
-start=`date +%s`
-echo "MPI PROGRAM STARTS FOR RANDOM DUMMY LOAD : $start "
-echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
-if (( $ppn == 0 ))
-then
-   mpiexec -n $proc_count -hostfile loadhosts $binary
-else
-    mpiexec -n $proc_count -ppn $ppn -hostfile loadhosts $binary
-fi
-end=`date +%s`
-runtime=$((end-start))
-echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
-echo "MPI PROGRAM ENDS : $end :: Total Time : $runtime"
-echo "======================================================================"
-
+binary=$1
+while :
+do
+    > randomload_out.tmp
+    numhosts=`shuf -i 6-16 -n 1`
+    ppn=4
+    proc_count=$(($ppn*$numhosts))
+    echo "Num hosts: $numhosts | ppn : $ppn | proc_count : $proc_count"
+    cat /users/btech/akashish/.eagle/livehosts.txt | sort -R | sort -R | head -n $numhosts > loadhosts
+    echo ""
+    echo ""
+    echo "Randomly Selected hosts for dummy load"
+    cat loadhosts
+    echo "======================================================================"
+    start=`date +%s`
+    echo "MPI PROGRAM STARTS FOR RANDOM DUMMY LOAD : $start "
+    echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+    if (( $ppn == 0 ))
+    then
+    mpiexec -n $proc_count -hostfile loadhosts $binary
+    else
+        mpiexec -n $proc_count -ppn $ppn -hostfile loadhosts $binary >> randomload_out.tmp
+    fi
+    end=`date +%s`
+    runtime=$((end-start))
+    echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+    echo "MPI PROGRAM ENDS : $end :: Total Time : $runtime"
+    echo "======================================================================"
+done
--- eagle/setup_scripts/do_it_once_global.sh
+++ eagle/setup_scripts/do_it_once_global.sh
@@ -4,7 +4,7 @@
 #     echo "ssh into csews$id for do_it_once"
 #     ssh -q -o ConnectTimeout=2 csews$id ~/UGP/eagle/do_it_once.sh
 # done
-for id in {1..90}
+for id in {1..60}
 do  
     echo "ssh into csews$id for do_it_once"
     ssh -q -o ConnectTimeout=2 csews$id uptime
